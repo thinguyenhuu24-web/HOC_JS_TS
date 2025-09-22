57e1fed0be6a50edcf95b873215d58d4e8529274
thinguyenhuu24-web <thi.nguyenhuu24@gmail.com>
2025-09-22 22:14:57 +0700

    update bai6.js

--- bai6/bai6.js
+++ bai6/bai6.js
@@ -1,3 +1,5 @@
+//Git sẽ quản lý source code của mình qua các lớp từ local -> staging -> repo (quản lý cuối cùng trên git)
+//commit -> xác nhận sự thay đổi của code -> staging -> push code lên repo
 PS C:\Users\PC> pwd -> hiển thị path
 PS C:\Users\PC> clear 
 PS C:\Users\PC> ls -> show các thư mục
@@ -5,3 +7,10 @@ PS C:\Users\PC> cd . nhân tab -> show folder cần mở
 PS C:\Users\PC\Desktop> mkdir tên_thư_mục -> tạo 1 thư mục con
 Có 2 cách đẩy code lên git hub. Dùng terminal  hoặc dùng githubdesktop
 PS C:\Users\PC\Desktop\REPO_JS_TS_BASIC\GitHub\HOC_JS_TS> git status -> show ra thư mụcclone đã được liên kết với thư mục trên github hay chưa?
+PS C:\Users\PC\Desktop\REPO_JS_TS_BASIC\GitHub\HOC_JS_TS> git log -> xem được commmit vừa đẩy lên repo
+
+Git add ten file thay doi
+Git commit -m “ten commit”
+Git push
+.gitignore
+PS C:\Users\PC\Desktop\REPO_JS_TS_BASIC\GitHub\HOC_JS_TS> git config --global --list -> xem thông git cài đặt trên github: git config --global --list
